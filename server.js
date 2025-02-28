8ff03ae11b52d0961f2ef9406c99e7a1758ab3d3
Al101020 <alex.vl.m@yandex.ru>
2025-02-28 17:19:45 +1000

    commit-1.4

--- server.js
+++ server.js
@@ -1,25 +1,112 @@
-
+const fs = require('fs');
 const http = require('http');
 const Koa = require('koa');
-const koaBody = require('koa-body');
+const { koaBody } = require('koa-body');
+const path = require('path');
 
 const app = new Koa();
+const subscriptions = [];
+
+app.use(koaBody({ // ошибку выдаёт, говорит что koaBody не функция, 
+// уже нет - импорт нужен { именованый },
+  urlencoded: true,
+  multipart: true,
+}));
 
 app.use((ctx, next) => { // ctx - типа context
-  console.log(ctx.request.body);
+  if (ctx.request.method !== 'OPTIONS') {
+    next();
 
-  ctx.response.body = 'SERVER response';
+    return;
+  }
+
+  ctx.response.set('Access-Control-Allow-Origin', '*');
+
+  ctx.response.set('Access-Control-Allow-Methods', 'DELETE, PUT, PATCH, GET, POST');
+
+  ctx.response.status = 204;
+});
+
+app.use((ctx, next) => { // ctx - типа context
+  if (ctx.request.method !== 'POST' && ctx.request.url !== '/upload') {
+    next();
+
+    return;
+  }
+
+  ctx.response.set('Access-Control-Allow-Origin', '*');
+
+  console.log(ctx.request.files);
+
+  try {
+    console.log(path);
+    const public = path.join(__dirname, '/public');
+  
+    const { file } = ctx.request.files;
+
+    fs.copyFileSync(file.path, public + '/' + file.name);
+
+  } catch (error) {
+    ctx.response.status = 501;
+
+    return;
+  }
+  
+  ctx.response.body = 'OK';
+});
+
+app.use((ctx, next) => { // ctx - типа context
+  if (ctx.request.method !== 'POST') {
+    next();
+
+    return;
+  }
+
+  // console.log(ctx.request.body);
+
+  const { name, phone } = ctx.request.body;
+
+  ctx.response.set('Access-Control-Allow-Origin', '*');
+  // ctx.response.set('Access-Control-Allow-Methods', 'DELETE, PUH, PATCH, GET, POST');
+  if (subscriptions.some(sub => sub.phone === phone)) {
+    ctx.response.status = 400;
+    ctx.response.body = 'subscription exists';
+
+    return;
+  }
+
+  subscriptions.push({ name, phone });
+  // ctx.response.body = 'SERVER response';
+  ctx.response.body = 'OK';
 
   next();
 });
 
-app.use(koaBody({ // ошибку выдаёт, говорит что koaBody не функция, версию надо ниже
-  urlencoded: true,
-}));
+app.use((ctx, next) => {
+  if (ctx.request.method !== 'DELETE') {
+    next();
 
-app.use((ctx) => {
-  console.log(ctx.request.body);
-  console.log('I am a second middleware');
+    return;
+  }
+
+  console.log(ctx.request.query);  // console.log('I am a second middleware');
+
+  const { phone } = ctx.request.query;
+
+  ctx.response.set('Access-Control-Allow-Origin', '*');
+
+  if (subscriptions.every(sub => sub.phone !== phone)) {
+    ctx.response.status = 400;
+    ctx.response.body = 'subscription doesn\'t exists';
+
+    return;
+  }
+
+  subscriptions = subscriptions.filter(sub => sub.phone !== phone);
+
+  ctx.response.body = 'OK';
+
+  next();
 });
 
 const server = http.createServer(app.callback());
